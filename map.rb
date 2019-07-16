e60d390606d7844cfb88f5c84720c7cd6677f664
froggy <chrisdetmering@gmail.com>
2019-07-16 07:45:07 -0700

    Made stack and queue classes

---
+++ map.rb
@@ -0,0 +1,38 @@
+class Map 
+    def initialize 
+        @underlying_array = []
+    end 
+
+    def set(key, value)
+        
+        
+        @underlying_array.each do |pair| 
+            
+        end 
+
+    end 
+     
+    def get(key)
+    end 
+
+    def delete(key)
+    end 
+
+    def show 
+    end 
+
+    
+    private
+
+    attr_reader :underlying_array
+
+    def has_key?(key)
+        @underlying_array.each do |pair| 
+            if key == pair[0]
+                return false
+            end 
+        end
+
+        false 
+    end
+end 
